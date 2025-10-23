0eee5f98d5b6d89c72b3452c13d0b9556e5667dd
RomaBirilo <gkdichfjf@gmail.com>
2025-10-23 18:50:51 +0300

    Завершена теоретическая часть

---
+++ Colloquium/task3/main.cpp
@@ -0,0 +1,43 @@
+﻿
+#include "main.h"
+#include "node.h"
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+int main()
+{
+	try
+	{
+		cout << "Input size of list:";
+		int n;
+		cin >> n;
+		if (n < 0)
+			throw invalid_argument("size of list must be > 0");
+		cout << "Input list:";
+		Node* head = nullptr;
+		int num;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> num;
+			head->push(head, num);
+		}
+
+		head = ReverseList(head);
+
+		Node* curr = head;
+		while (curr) {
+			cout << curr->data << " ";
+			curr = curr->next;
+		}
+	}
+	catch (const invalid_argument& er)
+	{
+		cout << er.what();
+	}
+	catch (const runtime_error& er)
+	{
+		cout << er.what();
+	}
+
+}
