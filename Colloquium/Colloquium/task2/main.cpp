968f59bc7fd2ae964b60945ac9c159e240301d37
RomaBirilo <gkdichfjf@gmail.com>
2025-10-23 18:22:39 +0300

    Для второй задачи добавлена обработка исключений

--- Colloquium/Colloquium/task2/main.cpp
+++ Colloquium/Colloquium/task2/main.cpp
@@ -1,27 +1,42 @@
 ﻿
 #include "main.h"
 #include <vector>
-
+#include <stdexcept>
 using namespace std;
 
 int main()
 {
-	cout << "Input size of your array:";
-	int size;
-	cin >> size;
-	vector <int> array_with_duplicates(size,0);
-	cout << "Input array:";
-	for (int i = 0; i < size; i++)
+	try
 	{
-		cin>>array_with_duplicates[i];
-	}
-	
-	vector<int> array_without_duplicates;
-	array_without_duplicates = RemoveRepetitions(array_with_duplicates);
+		cout << "Input size of your array:";
+		int size;
+		cin >> size;
+		if (size < 0)
+		{
+			throw invalid_argument("Array size must be positive.");
+		}
+		vector <int> array_with_duplicates(size, 0);
+		cout << "Input array:";
+		for (int i = 0; i < size; i++)
+		{
+			cin >> array_with_duplicates[i];
+			if (cin.fail()) {
+				throw invalid_argument("Invalid input. Please enter integers only.");
+			}
+		}
 
-	cout << "Result:";
-	for (int num : array_without_duplicates)
-	{
-		cout << num<<" ";
+		vector<int> array_without_duplicates;
+		array_without_duplicates = RemoveRepetitions(array_with_duplicates);
+
+		cout << "Result:";
+		for (int num : array_without_duplicates)
+		{
+			cout << num << " ";
+		}
 	}
+	catch (const invalid_argument& er)
+	{
+		cout << er.what();
+	}
+
 }
